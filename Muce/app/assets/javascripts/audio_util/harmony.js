0632cf0af590d534b3d7dacfcfb607d2679a47de
SamuSan <sambminns@gmail.com>
2015-05-02 21:11:10 +1200

    Wokring on harmony module, constructs chords and performs inversions now

--- Muce/app/assets/javascripts/audio_util/harmony.js
+++ Muce/app/assets/javascripts/audio_util/harmony.js
@@ -1,8 +1,42 @@
-$(function(){
-  window.HarmonyUtil = function(){
-    var MAJ = [0, 4, 7];
-    var MIN = [0, 3, 7];
+(function(){
+  var nameSeperator = '-';
+  var OCTAVE = 12;
+  var tonalities = {
+    'MAJ'   : [0, 4, 7],
+    'MAJ7'  : [0, 4, 7, 9],
+    'MAJ9'  : [0, 4, 7, 9, 11],
+    'DOM7'  : [0, 4, 7, 8, 11],
+    'MIN'   : [0, 3, 7],
+    'MIN7'  : [0, 3, 7, 9],
+    'MIN9'  : [0, 3, 7, 9, 11],
+    'MIN11' : [0, 3, 7, 9, 11, 13],
+    'DIM'   : [0, 3, 6],
+    'AUG'   : [0, 4, 8],};
 
-    
+  var HarmonyUtil = {
+
+    chordFromName: function (chordName) {
+      var note = chordName.split(nameSeperator)[0].toUpperCase();
+      var tonality = chordName.split(nameSeperator)[1].toUpperCase();
+      var noteNumber = MIDIUtils.noteNameToNoteNumber(note + '-4');
+
+      return constructChordNotes(noteNumber, tonality);
+    },
+
+    invertChord: function (chord) {
+      inverted = chord.slice(1);
+      inverted.push(chord[0] + OCTAVE);
+      return inverted;
+    } 
+  };
+
+  function constructChordNotes (noteNumber, tonality){
+    var chord = [];
+    tonalities[tonality].forEach(function(interval) {
+      chord.push(noteNumber + interval);
+    });
+    return chord;
   }
-});
+
+  window.HarmonyUtil = HarmonyUtil;
+}).call(this);
