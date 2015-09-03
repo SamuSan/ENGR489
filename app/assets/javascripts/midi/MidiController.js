180a7cdc02256edc19574bd1119aee2654dfc2d8
SamuSan <sambminns@gmail.com>
2015-09-04 09:43:11 +1200

    Fix javascript error in MidiController

--- app/assets/javascripts/midi/MidiController.js
+++ app/assets/javascripts/midi/MidiController.js
@@ -26,18 +26,18 @@ function MidiController(midiConnection) {
   }
 
   function listInputsAndOutputs( midiAccess ) {
-    for (var entry of midiAccess.inputs) {
-      var input = entry[1];
-      console.log( "Input port [type:'" + input.type + "'] id:'" + input.id +
-        "' manufacturer:'" + input.manufacturer + "' name:'" + input.name +
-        "' version:'" + input.version + "'" );
-    }
+    // for (var entry of midiAccess.inputs) {
+    //   var input = entry[1];
+    //   console.log( "Input port [type:'" + input.type + "'] id:'" + input.id +
+    //     "' manufacturer:'" + input.manufacturer + "' name:'" + input.name +
+    //     "' version:'" + input.version + "'" );
+    // }
 
-    for (var entry of midiAccess.outputs) {
-      var output = entry[1];
-      console.log( "Output port [type:'" + output.type + "'] id:'" + output.id +
-        "' manufacturer:'" + output.manufacturer + "' name:'" + output.name +
-        "' version:'" + output.version + "'" );
-    }
+    // for (var entry of midiAccess.outputs) {
+    //   var output = entry[1];
+    //   console.log( "Output port [type:'" + output.type + "'] id:'" + output.id +
+    //     "' manufacturer:'" + output.manufacturer + "' name:'" + output.name +
+    //     "' version:'" + output.version + "'" );
+    // }
   }
 }
