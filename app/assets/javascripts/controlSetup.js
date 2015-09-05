d19573e2d68f1dafc5249e07f9d5248170e9bbc5
SamuSan <sambminns@gmail.com>
2015-09-05 20:23:43 +1200

    Fix doc links

--- app/assets/javascripts/controlSetup.js
+++ app/assets/javascripts/controlSetup.js
@@ -11,8 +11,6 @@ $(function(){
     var input = $('#text-area').val();
     hopperWrapper = new HopperWrapper();
     hopperWrapper.interpret(input);
-    // t = new Test();
-    // t.startTest();
   });
 
   $('#stop-button').on('click', function(e){
@@ -24,8 +22,8 @@ $(function(){
 
   $('#man-button').on('click', function(e){
     e.preventDefault();
-    window.open("assets/gm_experimental_proceedure.pdf", '_blank');
-    window.open("assets/gm_docs.pdf", '_blank');
+    window.open(window.FileUtils.fileLocation("docs"), '_blank');
+    window.open(window.FileUtils.fileLocation("expproc"), '_blank');
   });
 
   $('#done-button').on('click', function(e){
