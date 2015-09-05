740e8b64db596672925c07578f4435778d24a4d1
SamuSan <sambminns@gmail.com>
2015-09-05 18:30:10 +1200

    Remove test input loading

--- app/assets/javascripts/util/testInputLoader.js
+++ app/assets/javascripts/util/testInputLoader.js
@@ -1,41 +1,41 @@
-$(function(){
-  var inputs  = [
-    "def s = Synth.name(\"s\") wave(\"triangle\") chord(\"C-MAJ7\")\n",
-    "s.setEnvelopeRelease(2.0)\n",
-    "s.insert(\"reverb\")\n",
-    "s.insert(\"distortion\")\n",
+// $(function(){
+//   var inputs  = [
+//     "def s = Synth.name(\"s\") wave(\"triangle\") chord(\"C-MAJ7\")\n",
+//     "s.setEnvelopeRelease(2.0)\n",
+//     "s.insert(\"reverb\")\n",
+//     "s.insert(\"distortion\")\n",
 
-    // "def dp = Part.instrument(s) rhythm(\"CR\")\n",
-    "def d = DrumMachine.name(\"rockbeat\")\n",
-    "def dp = Part.instrument(d) rhythm(\"ROCK_BEAT_ONE\")\n",
+//     // "def dp = Part.instrument(s) rhythm(\"CR\")\n",
+//     "def d = DrumMachine.name(\"rockbeat\")\n",
+//     "def dp = Part.instrument(d) rhythm(\"ROCK_BEAT_ONE\")\n",
 
-    // "def seq = Sequencer.parts(list.with(dp))\n",
-    "def lp = LoopPlayer.name(\"lp\" ) filename(\"sample_1\")\n",
-    "def lpprt = Part.instrument(lp) rhythm(\"ONE_BAR_LOOP\")\n",
-    "def seq = Sequencer.parts(list.with(dp, lpprt))\n",
-    "def p = Performance.instrument(s) sequencer(seq)\n",
-    ]
+//     // "def seq = Sequencer.parts(list.with(dp))\n",
+//     "def lp = LoopPlayer.name(\"lp\" ) filename(\"sample_1\")\n",
+//     "def lpprt = Part.instrument(lp) rhythm(\"ONE_BAR_LOOP\")\n",
+//     "def seq = Sequencer.parts(list.with(dp, lpprt))\n",
+//     "def p = Performance.instrument(s) sequencer(seq)\n",
+//     ]
 
-  // var inputs  = [
-  //   "def s = Synth.name(\"s\") wave(\"triangle\") chord(\"C-MAJ7\")\n",
-  // //   // "s.setEnvelopeRelease(2.0)\n",
-  // //   // "s.insert(\"reverb\")\n",
-  // //   // "s.insert(\"distortion\")\n",
+//   // var inputs  = [
+//   //   "def s = Synth.name(\"s\") wave(\"triangle\") chord(\"C-MAJ7\")\n",
+//   // //   // "s.setEnvelopeRelease(2.0)\n",
+//   // //   // "s.insert(\"reverb\")\n",
+//   // //   // "s.insert(\"distortion\")\n",
 
-  //   // // "def dp = Part.instrument(s) rhythm(\"CR\")\n",
-  // //   // "def d = DrumMachine.name(\"rockbeat\")\n",
-  // //   // "def dp = Part.instrument(d) rhythm(\"ROCK_BEAT_ONE\")\n",
+//   //   // // "def dp = Part.instrument(s) rhythm(\"CR\")\n",
+//   // //   // "def d = DrumMachine.name(\"rockbeat\")\n",
+//   // //   // "def dp = Part.instrument(d) rhythm(\"ROCK_BEAT_ONE\")\n",
 
-  // //   // "def seq = Sequencer.parts(dp)\n",
-  // //   "def lp = LoopPlayer.name(\"lp\" ) filename(\"sample_1.wav\")\n",
-  // //   "def lpprt = Part.instrument(lp) rhythm(\"ONE_BAR_LOOP\")\n",
-  // //   "def d = DrumMachine.name(\"rockbeat\")\n",
-  // //   "def dp = Part.instrument(d) rhythm(\"ROCK_BEAT_ONE\")\n",
-  // //   "def seq = Sequencer.parts( list.with(lpprt, dp) )\n",
-  //   // "def p = Performance.instrument(s) sequencer(\"\")\n",
-  //   ]
+//   // //   // "def seq = Sequencer.parts(dp)\n",
+//   // //   "def lp = LoopPlayer.name(\"lp\" ) filename(\"sample_1.wav\")\n",
+//   // //   "def lpprt = Part.instrument(lp) rhythm(\"ONE_BAR_LOOP\")\n",
+//   // //   "def d = DrumMachine.name(\"rockbeat\")\n",
+//   // //   "def dp = Part.instrument(d) rhythm(\"ROCK_BEAT_ONE\")\n",
+//   // //   "def seq = Sequencer.parts( list.with(lpprt, dp) )\n",
+//   //   // "def p = Performance.instrument(s) sequencer(\"\")\n",
+//   //   ]
 
-  inputs.forEach(function(input){
-    $('#text-area').append(input);
-  });
-})
+//   inputs.forEach(function(input){
+//     $('#text-area').append(input);
+//   });
+// })
